c5e79428756874f5e6af9c4dd39a656d433218a9
louisemogensenn <eaa23lmog@students.eaaa.dk>
2025-02-19 02:18:22 +0100

    Responsivt burgermenu

--- js/script.js
+++ js/script.js
@@ -10,3 +10,18 @@ forsidebillede.addEventListener('mouseover', () => { // En eventListener, der ve
 forsidebillede.addEventListener('mouseleave', () => { // En eventListener, der venter på at musen er over billedet. () angiver, at der køres en funktion, når betingelsen (eventListeneren) opfyldes og denne funktion er, angivet ved hjælp af pilesyntaxen =>, den funktion, der kommer under
     forsidebillede.src = "images/forsidebillede.webp"; // Når betingelsen er opfyldt ændres billedet kilde (source)
 });
+
+// Script til burgermenu
+
+const burgermenu = document.getElementById('burgermenu'); // Henter HTML-elementet med ID'et
+
+        const lukBurgermenu = document.getElementById('lukBurgermenu');
+
+        burgermenu.addEventListener('click', () => {
+            document.getElementById("navigationsmenu").classList.add("aktiv");
+        })
+
+        lukBurgermenu.addEventListener('click', () => {
+            document.getElementById("navigationsmenu").classList.remove("aktiv");
+        })
+    
